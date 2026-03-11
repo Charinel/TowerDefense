4e88206ab55cd74a6701b8c6cb92cd1f61b842e2
Charinel <sam040@hotmail.ca>
2026-03-11 15:19:15 -0400

    added save
    Le setting de son peux maintenant être sauvegarder

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -8,21 +8,48 @@ const GAME = preload("res://scene/level.tscn")
 
 var totalSong = 0
 var musicDirectory : DirAccess = DirAccess.open("res://Ressources/Music")
+var settingsFile = "res://Settings.txt" 
+var settingsContent :String
+
+var settingArray : PackedStringArray = []
+const BACKGROUNDMUSIC = 0
 
 func _ready() -> void:
 	fillPlaylist()
+	loadSettings()
 	var rondomNumber :int = randi() % totalSong
 	backgorund_music_slider.value = background_music.volume_linear
 	background_music.play() #TO DO add randomiser
 	
 func fillPlaylist() -> void:
 	if musicDirectory:
-		print(totalSong)
 		var songFilePath : PackedStringArray = musicDirectory.get_files()
 		totalSong = songFilePath.size()
-		print(totalSong)
-	pass
 
+func loadSettings() -> void:
+	if FileAccess.file_exists(settingsFile):
+		var file = FileAccess.open(settingsFile, FileAccess.READ)
+		settingsContent = file.get_as_text()
+		settingArray = settingsContent.split(";",true,0)
+		background_music.volume_linear = settingArray[BACKGROUNDMUSIC].to_float()
+		file.close()
+	else:
+		print("Error: File not found")
+		
+func saveSettings() -> void:
+	if FileAccess.file_exists(settingsFile):
+		settingsContent = ""
+		var file = FileAccess.open(settingsFile, FileAccess.WRITE)
+		print(settingArray.size())
+		for x in settingArray.size() - 1:
+			var temp = str(settingArray[x]) + ";"
+			print(settingArray[x])
+			print(temp)
+			settingsContent += temp
+		file.store_string(settingsContent)
+		file.close()
+	else:
+		print("Error: File not found")
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(GAME)
@@ -40,7 +67,10 @@ func _on_quit_pressed() -> void:
 func _on_backgorund_music_slider_drag_ended(value_changed: bool) -> void:
 	if value_changed :
 		background_music.volume_linear = backgorund_music_slider.value
-
+		print("Slider update")
+		print(str(background_music.volume_linear))
+		settingArray[BACKGROUNDMUSIC] = str(background_music.volume_linear)
+		saveSettings()
 
 func _on_back_pressed() -> void:
 	menuContainer.show()
