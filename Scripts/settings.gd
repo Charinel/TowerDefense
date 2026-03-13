5e09ef7c79318adb5630c38e7258657a45877fa4
Charinel <sam040@hotmail.ca>
2026-03-13 11:07:06 -0400

    Settings node
    Changement dans la lecture des settings une node s'occupe maintenant des settings et non les menus

---
+++ Scripts/settings.gd
@@ -0,0 +1,49 @@
+extends Control
+@onready var backgorund_music_slider: HSlider = $OptionsContainer/BackgorundMusicSlider
+@onready var back: Button = $OptionsContainer/Back
+
+var settingsFile = "res://Settings.txt" 
+var settingsContent :String
+
+var settingArray : PackedStringArray = [] # tableau qui contient la lecture du fichier
+const BACKGROUNDMUSIC = 0
+var parent
+
+func saveSettings() -> void:
+	if FileAccess.file_exists(settingsFile):
+		settingsContent = ""
+		var file = FileAccess.open(settingsFile, FileAccess.WRITE)
+		print(settingArray.size())
+		for x in settingArray.size() - 1:
+			var temp = str(settingArray[x]) + ";"
+			print(settingArray[x])
+			print(temp)
+			settingsContent += temp
+		file.store_string(settingsContent)
+		file.close()
+	else:
+		print("Error: File not found")
+		
+func loadSettings() -> Array:
+	var arrayOfSettings : Array = [] # tableau que l'on rempli des datas que l'on retourne au caller
+	if FileAccess.file_exists(settingsFile):
+		var file = FileAccess.open(settingsFile, FileAccess.READ)
+		settingsContent = file.get_as_text()
+		settingArray = settingsContent.split(";",true,0) 
+		arrayOfSettings.append(settingArray[BACKGROUNDMUSIC].to_float())
+		backgorund_music_slider.value = settingArray[BACKGROUNDMUSIC].to_float()
+		file.close()
+	else:
+		print("Error: File not found")
+	
+	return arrayOfSettings
+		
+func _on_backgorund_music_slider_drag_ended(value_changed: bool) -> void:
+	if value_changed :
+		parent.background_music.volume_linear = backgorund_music_slider.value
+		settingArray[BACKGROUNDMUSIC] = str(backgorund_music_slider.value)
+		saveSettings()
+
+func _on_back_pressed() -> void:
+	parent.menuContainer.show()
+	self.hide()
--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -3,22 +3,22 @@ const GAME = preload("res://scene/level.tscn")
 
 @onready var background_music: AudioStreamPlayer = $BackgroundMusic
 @onready var menuContainer: BoxContainer = $Control/MenuContainer
-@onready var optionsContainer: VBoxContainer = $Control/OptionsContainer
-@onready var backgorund_music_slider: HSlider = $Control/OptionsContainer/BackgorundMusicSlider
+@onready var settings: Control = $Control/Settings
+@onready var backgorund_music_slider: HSlider = settings.get_child(0).get_child(1)
+@onready var optionsContainer: VBoxContainer = settings.get_child(0)
+
 
 var totalSong = 0
 var musicDirectory : DirAccess = DirAccess.open("res://Ressources/Music")
-var settingsFile = "res://Settings.txt" 
-var settingsContent :String
-
-var settingArray : PackedStringArray = []
-const BACKGROUNDMUSIC = 0
+var settingArray : Array = []
+const BACKGROUNDVOLUME = 0
 
 func _ready() -> void:
 	fillPlaylist()
-	loadSettings()
+	settings.parent = self
+	settingArray = settings.loadSettings()
+	background_music.volume_linear = settingArray[BACKGROUNDVOLUME]
 	var rondomNumber :int = randi() % totalSong
-	backgorund_music_slider.value = background_music.volume_linear
 	background_music.play() #TO DO add randomiser
 	
 func fillPlaylist() -> void:
@@ -26,52 +26,12 @@ func fillPlaylist() -> void:
 		var songFilePath : PackedStringArray = musicDirectory.get_files()
 		totalSong = songFilePath.size()
 
-func loadSettings() -> void:
-	if FileAccess.file_exists(settingsFile):
-		var file = FileAccess.open(settingsFile, FileAccess.READ)
-		settingsContent = file.get_as_text()
-		settingArray = settingsContent.split(";",true,0)
-		background_music.volume_linear = settingArray[BACKGROUNDMUSIC].to_float()
-		file.close()
-	else:
-		print("Error: File not found")
-		
-func saveSettings() -> void:
-	if FileAccess.file_exists(settingsFile):
-		settingsContent = ""
-		var file = FileAccess.open(settingsFile, FileAccess.WRITE)
-		print(settingArray.size())
-		for x in settingArray.size() - 1:
-			var temp = str(settingArray[x]) + ";"
-			print(settingArray[x])
-			print(temp)
-			settingsContent += temp
-		file.store_string(settingsContent)
-		file.close()
-	else:
-		print("Error: File not found")
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(GAME)
 
-
 func _on_options_pressed() -> void:
 	menuContainer.hide()
-	optionsContainer.show()
-
+	settings.show()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_backgorund_music_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed :
-		background_music.volume_linear = backgorund_music_slider.value
-		print("Slider update")
-		print(str(background_music.volume_linear))
-		settingArray[BACKGROUNDMUSIC] = str(background_music.volume_linear)
-		saveSettings()
-
-func _on_back_pressed() -> void:
-	menuContainer.show()
-	optionsContainer.hide()
